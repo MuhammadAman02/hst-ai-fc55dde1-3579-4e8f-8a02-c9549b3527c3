443f7be38e70f693f4eaa44a02e35dc0cf800da2
MuhammadAman02 <mohammada@hst.ie>
2025-05-09 18:49:13 +0500

    update file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-lovable-dark text-white py-8">
+    <footer className="bg-gray-100 dark:bg-lovable-dark text-gray-800 dark:text-white py-8">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
           <p>&copy; 2023 Lovable Clone. All rights reserved.</p>
