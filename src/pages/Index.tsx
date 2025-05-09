51dd17694825d544caa4bf6d51248f6611b88321
MuhammadAman02 <mohammada@hst.ie>
2025-05-09 18:48:45 +0500

    update file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,10 +3,12 @@ import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Testimonial from '../components/Testimonial';
 import Footer from '../components/Footer';
+import ThemeToggle from '../components/ThemeToggle';
 
 const Index: React.FC = () => {
   return (
-    <div className="min-h-screen bg-lovable-dark">
+    <div className="min-h-screen bg-white dark:bg-lovable-dark transition-colors duration-300">
+      <ThemeToggle />
       <Hero />
       <Features />
       <Testimonial />
